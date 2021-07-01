29b85e4d4dc0dc8e5f065eb286f1617d1504d11f
GabrielT <gabriel0torre@gmail.com>
2021-07-01 20:28:13 -0300

    Plays random chess

--- board.py
+++ board.py
@@ -1,10 +1,20 @@
 import chess
+import random as rn
 
 def main():
-  board = chess.Board()
-  print(board.generate_legal_moves())
+  board =  chess.Board()
 
 
+  print(board)
+
+  print(board.legal_moves)
+
+  while not board.is_game_over():
+
+    nextMove = rn.choice(list(board.legal_moves))
+    board.push(nextMove)
+    print("----------\nply: %d \n" %(board.ply()))
+    print(board)
 
 
 if __name__ == "__main__":
